b6f3af2665a0b5b2c7b148a2bfa0c8196a175603
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-12 12:38:48 -0700

    Completed calculator.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(a, b) 
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  total = 0
+  array.each { |x| total += x }
+  total
+end
+
+def multiply(*args)
+  total = 1
+  args.each { |x| total *= x }
+  total
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(n)
+  total = 1
+  (1..n).each do |i|
+    total *= i
+  end
+  total
+end
